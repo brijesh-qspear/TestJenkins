2ec16cc8488f3fb81da041553083260abeae8e74
seemantkumar <seemant.kumar@sapple.co.in>
2017-08-08 14:15:12 +0530

    ok

---
+++ AppJenkins1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppJenkins1.Startup))]
+namespace AppJenkins1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
